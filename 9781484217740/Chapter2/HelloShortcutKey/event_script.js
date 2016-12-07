be7f2997f0fbb5c5ec699c9460ec551b86b51deb
Holly <hollies@hollies.pw>
2016-12-07 14:58:10 -0800

    Change to toggle Toggle icon also on button click.

--- 9781484217740/Chapter2/HelloShortcutKey/event_script.js
+++ 9781484217740/Chapter2/HelloShortcutKey/event_script.js
@@ -1,16 +1,40 @@
 //region {variables and functions}
 var consoleGreeting = "Hello World! - from event_script.js";
-var details = {"path":"icon-2.png"};
+
 //end-region
 
+var currentIcon = 1;
 
+function changeIcon() {
+	var details;
+
+	if (currentIcon == 1) {
+		details = {
+			"path": "icon-2.png"
+		};
+		currentIcon = 2;
+	} else {
+		details = {
+			"path": "icon-1.png"
+		};
+		currentIcon = 1;
+	}
+
+	console.log("changeIcon: " + JSON.stringify(details));
+	chrome.browserAction.setIcon(details, function () { 
+		console.log("icon changed");
+	});
+}
 
 //region {calls}
 console.log(consoleGreeting);
-chrome.commands.onCommand.addListener(function(command) {
-	chrome.browserAction.setIcon(
-		details,
-		function() {/**/}
-	);
+chrome.commands.onCommand.addListener(function () {
+	console.log("command received");
+	changeIcon();
+});
+
+chrome.browserAction.onClicked.addListener(function () {
+	console.log("click received");
+	changeIcon();
 });
 //end-region
